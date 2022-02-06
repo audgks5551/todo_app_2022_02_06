0ea17b3f8ad00b89067d9881beac2e642525278c
audgks5551 <audgks5551@daum.net>
2022-02-06 12:26:09 +0900

    컨트롤러 사용 연습

---
+++ src/main/java/com/example/apidemo/dto/TodoDTO.java
@@ -0,0 +1,25 @@
+package com.example.apidemo.dto;
+
+import com.example.apidemo.model.TodoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+
+    private String id;
+    private String title;
+    private boolean done;
+    // userid를 적지 않은 이유는 보안상 숨겨야하기 때문
+
+    public TodoDTO(final TodoEntity entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.done = entity.isDone();
+    }
+}
