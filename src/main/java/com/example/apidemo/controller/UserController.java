14f1bc151f282eef9c508e799b77a25d19d17e58
audgks5551 <audgks5551@daum.net>
2022-02-14 13:50:16 +0900

    유저부분부터 다 주석처리

--- src/main/java/com/example/apidemo/controller/UserController.java
+++ src/main/java/com/example/apidemo/controller/UserController.java
@@ -1,79 +1,79 @@
-package com.example.apidemo.controller;
-
-import com.example.apidemo.dto.ResponseDTO;
-import com.example.apidemo.dto.UserDTO;
-import com.example.apidemo.model.UserEntity;
-import com.example.apidemo.security.TokenProvider;
-import com.example.apidemo.service.UserService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-@RequestMapping("/auth")
-public class UserController {
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private TokenProvider tokenProvider;
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
-        try {
-            UserEntity user = UserEntity.builder()
-                    .email(userDTO.getEmail())
-                    .username(userDTO.getUsername())
-                    .password(userDTO.getPassword())
-                    .build();
-
-            UserEntity registeredUser = userService.create(user);
-            UserDTO responseUserDTO = UserDTO.builder()
-                    .email(registeredUser.getEmail())
-                    .id(registeredUser.getId())
-                    .username(registeredUser.getUsername())
-                    .build();
-            return ResponseEntity.ok().body(responseUserDTO);
-        } catch (Exception e) {
-
-            ResponseDTO responseDTO = ResponseDTO.builder()
-                    .error(e.getMessage())
-                    .build();
-            return ResponseEntity
-                    .badRequest()
-                    .body(responseDTO);
-        }
-    }
-
-    @PostMapping("/signin")
-    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
-        UserEntity user = userService.getByCredentials(
-                userDTO.getEmail(),
-                userDTO.getPassword()
-        );
-
-        if(user != null) {
-            final String token = tokenProvider.create(user);
-            final UserDTO responseUserDTO = UserDTO.builder()
-                    .email(user.getEmail())
-                    .id(user.getId())
-                    .token(token)
-                    .build();
-            return ResponseEntity.ok().body(responseUserDTO);
-        } else {
-            ResponseDTO responseDTO = ResponseDTO.builder()
-                    .error("Login failed.")
-                    .build();
-
-            return ResponseEntity
-                    .badRequest()
-                    .body(responseDTO);
-        }
-    }
-}
+//package com.example.apidemo.controller;
+//
+//import com.example.apidemo.dto.ResponseDTO;
+//import com.example.apidemo.dto.UserDTO;
+//import com.example.apidemo.model.UserEntity;
+//import com.example.apidemo.security.TokenProvider;
+//import com.example.apidemo.service.UserService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+////@RestController
+//@RequestMapping("/auth")
+//public class UserController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private TokenProvider tokenProvider;
+//
+//    @PostMapping("/signup")
+//    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+//        try {
+//            UserEntity user = UserEntity.builder()
+//                    .email(userDTO.getEmail())
+//                    .username(userDTO.getUsername())
+//                    .password(userDTO.getPassword())
+//                    .build();
+//
+//            UserEntity registeredUser = userService.create(user);
+//            UserDTO responseUserDTO = UserDTO.builder()
+//                    .email(registeredUser.getEmail())
+//                    .id(registeredUser.getId())
+//                    .username(registeredUser.getUsername())
+//                    .build();
+//            return ResponseEntity.ok().body(responseUserDTO);
+//        } catch (Exception e) {
+//
+//            ResponseDTO responseDTO = ResponseDTO.builder()
+//                    .error(e.getMessage())
+//                    .build();
+//            return ResponseEntity
+//                    .badRequest()
+//                    .body(responseDTO);
+//        }
+//    }
+//
+//    @PostMapping("/signin")
+//    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+//        UserEntity user = userService.getByCredentials(
+//                userDTO.getEmail(),
+//                userDTO.getPassword()
+//        );
+//
+//        if(user != null) {
+//            final String token = tokenProvider.create(user);
+//            final UserDTO responseUserDTO = UserDTO.builder()
+//                    .email(user.getEmail())
+//                    .id(user.getId())
+//                    .token(token)
+//                    .build();
+//            return ResponseEntity.ok().body(responseUserDTO);
+//        } else {
+//            ResponseDTO responseDTO = ResponseDTO.builder()
+//                    .error("Login failed.")
+//                    .build();
+//
+//            return ResponseEntity
+//                    .badRequest()
+//                    .body(responseDTO);
+//        }
+//    }
+//}
